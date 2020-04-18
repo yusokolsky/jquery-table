eb472efdb1e11715038bd4c0dfdc45990b20cd2d
yusokolsky <yusokolsky@gmail.com>
2020-04-18 17:38:27 +0300

    Transferring data to a server and writing to a file ready

--- Yuriy Sokolsky HW2/index.js
+++ Yuriy Sokolsky HW2/index.js
@@ -1,10 +1,36 @@
 let express = require('express');
 let app = express();
+var bodyParser = require('body-parser');
+var jsonfile = require('jsonfile');
+app.use(bodyParser.json());
 app.use(express.static('public'));
 app.set('port', process.env.PORT || 80);
-
+let file='Products.json';
+let Products=[];
 let server = app.listen(app.get('port'), function() { //identification
 
     console.log('Сервер запущен на  http://localhost:' + app.get('port') + '  Ctrl-C to terminate');
 
+    jsonfile.readFile(file, function(err, obj) {Products=obj})
 });
+
+app.post('/sendProduct/', function(req, res) {
+
+   let Product=req.body;
+    Product.id=Products.length;
+    Product.count=parseInt(Product.count);
+    Product.price=parseFloat(Product.price.slice(1));
+    Product.delivery={
+        country:Product.country,
+        cities:[Product.city1,Product.city2,Product.city3]
+    }
+    delete Product.country;
+    delete Product.city1;
+    delete Product.city2;
+    delete Product.city3;
+    Products.push(Product);
+    jsonfile.writeFile(file, Products);
+
+    res.end();
+});
+
--- Yuriy Sokolsky HW2/public/scripts/script.js
+++ Yuriy Sokolsky HW2/public/scripts/script.js
@@ -1,8 +1,35 @@
+function ajaxReq(url, data, callback){
+    $.ajax({
+        url: url,
+        type: 'POST',
+        contentType:'application/json',
+        data: JSON.stringify(data),
+        dataType:'json'
+    }).done(callback);
+}
+
+$.fn.serializeFormJSON = function () {
+
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
 $('#openAddNew').on("click", function() {
     $('#modalBackround').show();
     $('.modalDialogEdit').show();
 })
-$('.closeModalDialog').on("click", function() {
+$('.closeModalDialog,#Cancel').on("click", function() {
     $('#modalBackround').hide();
     $('.modalDialogAlert').hide();
     $('.modalDialogEdit').hide();
@@ -25,30 +52,47 @@ $("#checkAll").click(function() {
 });
 
 $("#selCountry").change(function() {
+    $("#checkboxGroup").removeClass("d-none");
     Countries = [$("#city1"), $("#city2"), $("#city3")]
     switch ($("#selCountry option:selected").text()) {
         case "Russia":
             Countries[0].siblings('label').html('Saratov');
+            Countries[0].attr('value', 'Saratov');
             Countries[1].siblings('label').html('Moscow');
+            Countries[1].attr('value', 'Moscow');
             Countries[2].siblings('label').html('St. Petersburg');
+            Countries[2].attr('value', 'St. Petersburg');
             break;
         case "Belorus":
             Countries[0].siblings('label').html('Minsk');
+            Countries[0].attr('value', 'Minsk');
             Countries[1].siblings('label').html('Grodno');
+            Countries[1].attr('value', 'Grodno');
             Countries[2].siblings('label').html('Vitebsk');
+            Countries[2].attr('value', 'Vitebsk');
             break;
         case "USA":
             Countries[0].siblings('label').html('Washington');
+            Countries[0].attr('value', 'Washington');
             Countries[1].siblings('label').html('New York');
+            Countries[1].attr('value', 'New York');
             Countries[2].siblings('label').html('Seatle');
+            Countries[2].attr('value', 'Seatle');
             break;
-        default:
-        // code block
+        default: $("#checkboxGroup").addClass("d-none");
+
     }
 });
 
-$("#SaveChangesButton").click(function() {
 
+$('form[name="Product"]').submit(function(){
+
+    let Product= $(this).serializeFormJSON();
+    console.log(Product);
+    ajaxReq("/sendProduct/",Product , function(result) {
+
+    });
+    return false;
 });
 
 $("#productName").change(function() {
